273171c6ef5a31ca8be81327e0b5ddfdae9f7780
Cayden734 <csc1095@stacmail.net>
2026-02-09 11:41:13 +1300

    Monday w2 work

---
+++ child or adult prices.py
@@ -0,0 +1,13 @@
+
+AEG = 13
+
+age = int(input("Welcome to the Zoo Please enter your age"))
+
+if age < AEG:
+    print("You Pay Child Prices")
+else:
+     print("You Pay Adult Prices")
+
+print("Welcome to the Zoo")
+ 
+
---
+++ if statments.py
@@ -0,0 +1,11 @@
+''' Demonstrating how a conditinal statment (if/else) works
+It asks the user for a password and then checks if it is correct'''
+
+#Ask for password and store in a variable
+
+password = input("enter your password")
+# Cheak if the password is correct
+if password == "7165": 
+    print("Welcome") 
+else:
+    print("Incorrect")
