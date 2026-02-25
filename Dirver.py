7197e3d412cb801cf6f72a404c85f54a2578a71e
Cayden734 <csc1095@stacmail.net>
2026-02-26 12:40:13 +1300

    Update Dirver.py

--- Dirver.py
+++ Dirver.py
@@ -1,4 +1,5 @@
 
+
 name = ["Henry", "Rory", "Lucas", "Terence"]
 drive = ["No licence", "No licence", "Learners", "Restricted"]
 quit = 1
@@ -7,14 +8,33 @@ while quit != 0:
     print("=" * 25)
     for i in range(len(name)):
         print(f"{i+1}. {name[i]:10} {drive[i]:10}")
-    selection = int(input("Select Student to update:"))
+
+    # Get selection and error prevention
+    get_selection = True
+    while get_selection == True:
+        try:
+            selection = int(input("Select Student to update:"))
+            if selection < 0 or selection > len(name):
+                print("Invalid number")
+            else:
+                get_selection = False
+
+        except ValueError:
+            print("Invalid data type")
+
     if selection == 0:
         quit = 0
     else:
         drive2 = input("Enter new status")
-        drive[selection-1] = drive2
-    if selection == 4:
-        name = ["Henry", "Rory", "Lucas", "Diddy"]
+        if drive2 in drive:
+            drive[selection-1] = drive2
+            if selection == 4:
+                name = ["Henry", "Rory", "Lucas", "Diddy"]
+        else:
+            print("Incorrect Status")
+                
+
+        
    
   
     
